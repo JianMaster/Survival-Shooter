8e9b994cfba7db991d8399ed0ae40188fd776a2a
JianMaster <jianmaster97@gmail.com>
2019-04-22 09:12:34 +0800

    unity project

---
+++ Assets/Scripts/Managers/EnemyManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class EnemyManager : MonoBehaviour
+{
+    private PlayerHealth playerHealth;
+
+    public GameObject[] enemy;
+    public float spawnTime = 3f;
+    public Transform[] spawnPoints;
+
+
+    void Start ()
+    {
+        playerHealth = GameObject.FindWithTag("Player").GetComponent<PlayerHealth>();
+        InvokeRepeating("Spawn", spawnTime, spawnTime);
+    }
+
+
+    void Spawn ()
+    {
+        if(playerHealth.currentHealth <= 0f)
+        {
+            return;
+        }
+
+        int spawnPointIndex = Random.Range (0, spawnPoints.Length);
+        int enemyIndex = Random.Range(0, enemy.Length);
+
+        Instantiate (enemy[enemyIndex], spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
+    }
+}
---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 6f;
+    private Vector3 movement;
+    private Animator anim;
+    private Rigidbody rb;
+    private int floorMask;
+    private float cameraRay=100f;
+
+    private void Awake()
+    {
+        floorMask = LayerMask.GetMask("Floor");
+        anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+
+        Move(h, v);
+        Turning();
+        Animating(h, v);
+
+    }
+
+    private void Move(float h,float v)
+    {
+        movement.Set(h, 0.0f, v);
+        movement = movement.normalized * speed * Time.deltaTime;
+
+        rb.MovePosition(transform.position + movement);
+    }
+
+    private void Turning()
+    {
+        Ray camRay = Camera.main.ScreenPointToRay(Input .mousePosition);
+        RaycastHit floorHit;
+
+        if(Physics .Raycast(camRay,out floorHit,floorMask))
+        {
+            Vector3 playerToMouse = floorHit.point - transform.position;
+            playerToMouse.y = 0f;
+
+            Quaternion newRotation = Quaternion.LookRotation(playerToMouse);
+            rb.MoveRotation(newRotation);
+
+        }
+    }
+
+    private void Animating(float h,float v)
+    {
+        bool walking = (h != 0 || v != 0);
+        anim.SetBool("IsWalking", walking);
+    }
+}
